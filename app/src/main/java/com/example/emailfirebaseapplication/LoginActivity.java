dfbb176cce19c0de39fc0bba18c94366b56a0399
Fitsum A <fitsumcs@gmail.com>
2020-11-04 00:54:41 -0900

    Login done

--- app/src/main/java/com/example/emailfirebaseapplication/LoginActivity.java
+++ app/src/main/java/com/example/emailfirebaseapplication/LoginActivity.java
@@ -1,14 +1,67 @@
 package com.example.emailfirebaseapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Get Firebase auth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        // if user logged in, go to sign-in screen
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, LogedInActivity.class));
+            finish();
+        }
+
+        inputEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        inputPassword = (EditText) findViewById(R.id.editTextTextPassword);
+    }
+
+    public void loginUser(View view) {
+        String email = inputEmail.getText().toString();
+        final String password = inputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "All Fields Required!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (!task.isSuccessful()) {
+
+                        Toast.makeText(LoginActivity.this, "User Name or Password Incorrect!!", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        });
     }
 }
