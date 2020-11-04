6d5a7eb25ff38770ad0b8fef0e652cb48c862049
Fitsum A <fitsumcs@gmail.com>
2020-11-04 00:44:21 -0900

    Logout done

--- app/src/main/java/com/example/emailfirebaseapplication/LogedInActivity.java
+++ app/src/main/java/com/example/emailfirebaseapplication/LogedInActivity.java
@@ -2,13 +2,27 @@ package com.example.emailfirebaseapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogedInActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loged_in);
+
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
